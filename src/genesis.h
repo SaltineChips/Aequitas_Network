579ce88ccb0b35f5c985afeb09093e7755aba853
SaltineChips <rubixblockchain@gmail.com>
2019-05-18 12:47:29 -0700

    Update Genesis.h

--- src/genesis.h
+++ src/genesis.h
@@ -16,18 +16,18 @@ static const unsigned int timeTestNetGenesis = 1558180800+30; // Saturday, May 1
 /** Genesis RegNet Start Time */
 static const unsigned int timeRegNetGenesis = 1558180800+90; // Saturday, May 18, 2019 12:01:30 PM
 /** Genesis Nonce Mainnet*/
-static const unsigned int nNonceMain = 0;
+static const unsigned int nNonceMain = 49778;
 /** Genesis Nonce Testnet */
 static const unsigned int nNonceTest = 0;
 /** Genesis Nonce Regnet */
 static const unsigned int nNonceReg = 8;
 /** Main Net Genesis Block */
-static const uint256 nGenesisBlock("0x");
+static const uint256 nGenesisBlock("0x0000c552112fe0a5ee2997139e791bf2fa9c15ced39385ed5756d7ca43531b62");
 /** Test Net Genesis Block */
 static const uint256 hashTestNetGenesisBlock("0x");
 /** Reg Net Genesis Block */
 static const uint256 hashRegNetGenesisBlock("0x");
 /** Genesis Merkleroot */
-static const uint256 nGenesisMerkle("0x");
+static const uint256 nGenesisMerkle("0x7a5fd9fc575ec5ebee9e2d9ece344ffb75e5bfe0917bbc91ba260cfa1db094e1");
 
 #endif // BITCOIN_GENESIS_H
